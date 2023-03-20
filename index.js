44613808fdd280e122b4d84e49844726c2cc29ac
ClintTheEngineer <reachme@clintalexander.dev>
2023-03-20 16:34:46 -0400

    calendar update

--- index.js
+++ index.js
@@ -2,22 +2,23 @@
 
 const currentDate = new Date();
 const currentYear = currentDate.getFullYear();
-const month = currentDate.getMonth()
+const monthIndex = currentDate.getMonth()
 const monthsArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const leapDay = (currentYear % 4 == 0 && currentYear % 100 != 0 || currentYear % 400 == 0) ? 29 : 28;
 const daysArray = [31,leapDay,31,30,31,30,31,31,30,31,30,31];
 const date = currentDate.getDate();
 Number.prototype.monthNumbers = function(){
-    let month = [];
+    let monthIndex = [];
     let i = 1;
-    while(month.length < this){
-        month.push(i);
+    while(monthIndex.length < this){
+        monthIndex.push(i);
         i++
     }
-    return month;
+    return monthIndex;
 }
-
+const month = monthsArray[monthIndex];
 const year = new Object;
 for(let months in monthsArray){
     year[monthsArray[months]] = daysArray[months].monthNumbers();
 }
+
